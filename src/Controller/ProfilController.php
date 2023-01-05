6f3a213a51c2c3a62499f6f5590f2b550c9b5d8a
MichelC <a@a.a>
2023-01-05 13:09:59 +0100

    Création de la page profil

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilController extends AbstractController
+{
+
+    /**
+     * Contrôleur de la page de profil
+     * Accès réservé aux connectés (ROLE_USER)
+     */
+    #[Route('/mon-profil', name: 'main_profil')]
+    #[IsGranted('ROLE_USER')]
+    public function profil(): Response
+    {
+        return $this->render('main/profil.html.twig');
+    }
+}
